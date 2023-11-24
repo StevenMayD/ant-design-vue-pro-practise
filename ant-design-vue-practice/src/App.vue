16059b3d3a85bb05e53cb0e4a972cd9908daed80
dong_shuai_wen <dongshuaiwenchina@163.com>
2023-11-24 15:23:48 +0800

    主体布局优化

--- ant-design-vue-practice/src/App.vue
+++ ant-design-vue-practice/src/App.vue
@@ -1,74 +1,109 @@
 <template>
   <a-layout style="min-height: 100vh">
-    <a-layout-sider v-model:collapsed="collapsed" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <pie-chart-outlined />
-          <span>Option 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <desktop-outlined />
-          <span>Option 2</span>
-        </a-menu-item>
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined />
-              <span>User</span>
-            </span>
-          </template>
-          <a-menu-item key="3">Tom</a-menu-item>
-          <a-menu-item key="4">Bill</a-menu-item>
-          <a-menu-item key="5">Alex</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <team-outlined />
-              <span>Team</span>
-            </span>
-          </template>
-          <a-menu-item key="6">Team 1</a-menu-item>
-          <a-menu-item key="8">Team 2</a-menu-item>
-        </a-sub-menu>
-        <a-menu-item key="9">
-          <file-outlined />
-          <span>File</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
+    <!-- 设置header高度 -->
+    <a-layout-header
+      style="
+        background: #141414;
+        height: 48px;
+        padding: 0px;
+        vertical-align: middle;
+      "
+    >
+      <div class="logo">
+        <!-- 将svg作为字符串，来使用图片资源 -->
+        <img
+          :src="antdesign"
+          alt=""
+          style="padding-top: 8px; padding-left: 14px; vertical-align: top"
+        />
+        <!-- css样式 -->
+        <span style="color: #fff; padding: 10px; vertical-align: middle">
+          Ant Design Pro
+        </span>
+      </div>
+    </a-layout-header>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0" />
-      <a-layout-content style="margin: 0 16px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>User</a-breadcrumb-item>
-          <a-breadcrumb-item>Bill</a-breadcrumb-item>
-        </a-breadcrumb>
-        <div style="padding: 24px; background: #fff; min-height: 360px">
-          Bill is a cat.
-        </div>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
+      <a-layout-sider v-model:collapsed="collapsed" theme="light" collapsible>
+        <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <DashboardOutlined />
+                <span>仪表盘</span>
+              </span>
+            </template>
+            <a-menu-item key="1">分析页</a-menu-item>
+            <a-menu-item key="2">监控页</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <FormOutlined />
+                <span>表单</span>
+              </span>
+            </template>
+            <a-menu-item key="3">基础表单</a-menu-item>
+            <a-menu-item key="4">分布表单</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <TableOutlined />
+                <span>列表</span>
+              </span>
+            </template>
+            <a-menu-item key="5">搜索列表</a-menu-item>
+            <a-menu-item key="6">查询表格</a-menu-item>
+            <a-menu-item key="7">标准列表</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub4">
+            <template #title>
+              <span>
+                <team-outlined />
+                <span>详情页</span>
+              </span>
+            </template>
+            <a-menu-item key="8">基础详情页</a-menu-item>
+            <a-menu-item key="9">高级详情页</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="10">
+            <file-outlined />
+            <span>结果页</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-content style="margin: 0 16px">
+          <a-breadcrumb style="margin: 16px 0">
+            <a-breadcrumb-item>User</a-breadcrumb-item>
+            <a-breadcrumb-item>Bill</a-breadcrumb-item>
+          </a-breadcrumb>
+          <div style="padding: 24px; background: #fff; min-height: 360px">
+            Bill is a cat.
+          </div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          Ant Design ©2018 Created by Ant UED
+        </a-layout-footer>
+      </a-layout>
     </a-layout>
   </a-layout>
 </template>
 <script>
 import {
-  PieChartOutlined,
-  DesktopOutlined,
-  UserOutlined,
+  DashboardOutlined,
+  FormOutlined,
+  TableOutlined,
   TeamOutlined,
   FileOutlined,
 } from "@ant-design/icons-vue";
 import { defineComponent, ref } from "vue";
+import antdesign from "@/assets/antdesign.svg"; // 将svg作为字符串，来使用图片资源
 export default defineComponent({
   components: {
-    PieChartOutlined,
-    DesktopOutlined,
-    UserOutlined,
+    DashboardOutlined,
+    FormOutlined,
+    TableOutlined,
     TeamOutlined,
     FileOutlined,
   },
@@ -76,15 +111,18 @@ export default defineComponent({
     return {
       collapsed: ref(false),
       selectedKeys: ref(["1"]),
+      antdesign,
     };
   },
 });
 </script>
 <style>
-#components-layout-demo-side .logo {
-  height: 32px;
-  margin: 16px;
-  background: rgba(255, 255, 255, 0.3);
+.logo {
+  height: 48px;
+  /* margin: 0px; */
+  padding-top: 0px;
+  /* background: rgba(255, 255, 255, 0.3); */
+  background: #141414;
 }
 
 .site-layout .site-layout-background {
