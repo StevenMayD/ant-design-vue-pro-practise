c2a552aa87440599ee0f99aaa647329fda684c40
dong_shuai_wen <dongshuaiwenchina@163.com>
2023-11-24 16:43:22 +0800

    添加路由，迁移主体布局代码到basic-layout中

---
+++ ant-design-vue-practice/src/layouts/basic-layout.vue
@@ -0,0 +1,138 @@
+<template>
+  <a-layout style="min-height: 100vh">
+    <!-- 设置header高度 -->
+    <a-layout-header
+      style="
+        display: flex;
+        align-items: center;
+        height: 48px;
+        padding-left: 14px;
+      "
+    >
+      <div class="logo">
+        <!-- 将svg作为字符串，来使用图片资源 -->
+        <img :src="antdesign" />
+        <!-- css样式 -->
+        <span
+          style="
+            color: #fff;
+            font-size: large;
+            height: 48px;
+            display: flex;
+            align-items: center;
+            padding-left: 10px;
+          "
+        >
+          Ant Design Pro
+        </span>
+      </div>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider v-model:collapsed="collapsed" theme="light" collapsible>
+        <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <DashboardOutlined />
+                <span>仪表盘</span>
+              </span>
+            </template>
+            <a-menu-item key="1">分析页</a-menu-item>
+            <a-menu-item key="2">监控页</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <FormOutlined />
+                <span>表单</span>
+              </span>
+            </template>
+            <a-menu-item key="3">基础表单</a-menu-item>
+            <a-menu-item key="4">分布表单</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <TableOutlined />
+                <span>列表</span>
+              </span>
+            </template>
+            <a-menu-item key="5">搜索列表</a-menu-item>
+            <a-menu-item key="6">查询表格</a-menu-item>
+            <a-menu-item key="7">标准列表</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub4">
+            <template #title>
+              <span>
+                <team-outlined />
+                <span>详情页</span>
+              </span>
+            </template>
+            <a-menu-item key="8">基础详情页</a-menu-item>
+            <a-menu-item key="9">高级详情页</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="10">
+            <file-outlined />
+            <span>结果页</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-content style="margin: 0 16px">
+          <a-breadcrumb style="margin: 16px 0">
+            <a-breadcrumb-item>User</a-breadcrumb-item>
+            <a-breadcrumb-item>Bill</a-breadcrumb-item>
+          </a-breadcrumb>
+          <div style="padding: 24px; background: #fff; min-height: 360px">
+            Bill is a cat.
+          </div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          Ant Design ©2018 Created by Ant UED
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import {
+  DashboardOutlined,
+  FormOutlined,
+  TableOutlined,
+  TeamOutlined,
+  FileOutlined,
+} from "@ant-design/icons-vue";
+import { defineComponent, ref } from "vue";
+import antdesign from "@/assets/antdesign.svg"; // 将svg作为字符串，来使用图片资源
+export default defineComponent({
+  components: {
+    DashboardOutlined,
+    FormOutlined,
+    TableOutlined,
+    TeamOutlined,
+    FileOutlined,
+  },
+  data() {
+    return {
+      collapsed: ref(false),
+      selectedKeys: ref(["1"]),
+      antdesign,
+    };
+  },
+});
+</script>
+<style>
+.logo {
+  height: 48px;
+  display: flex;
+  align-items: center;
+  background: #141414;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+[data-theme="dark"] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
--- ant-design-vue-practice/src/router/index.js
+++ ant-design-vue-practice/src/router/index.js
@@ -1,20 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import BasicLayout from "../layouts/basic-layout.vue";
 
 const routes = [
   {
+    // 默认地址
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "BasicLayout",
+    component: BasicLayout,
   },
 ];
 
